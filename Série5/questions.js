70be484fb962f9ae0b376a9045ccbe8247636221
Antoni Dalle Nogare <antoni.dallenogare@gmail.com>
2018-05-25 16:02:41 +0200

    25/05 16h00

---
+++ Série5/questions.js
@@ -0,0 +1,64 @@
+/**
+ * Exercice sur les chaines de caractères.
+ * Trouvez la façon de faire la plus optimal.
+ * Il peut y avoir plusieur façon de faire.
+ */
+var tailleString = function (texte) {
+return texte.length;
+}
+var remplaceECar = function (texte) {
+return texte.replace("e", " ");
+}
+var concatString = function (texte1, texte2) {
+return texte1.concat(texte2)
+}
+var afficherCar5 = function (texte) {
+return texte.charAt(4);
+}
+var afficher9Car = function (texte) {
+return texte.substring(0,9);
+}
+var majusculeString = function (texte) {
+return texte.toUpperCase();
+}
+var minusculeString = function (texte) {
+return texte.toLowerCase();
+}
+var SupprEspaceString = function (texte) {
+return texte.trim();
+}
+var IsString = function (texte) {
+
+}
+
+var AfficherExtensionString = function (texte) {
+
+}
+var NombreEspaceString = function (texte) {
+
+}
+var InverseString = function (texte) {
+
+}
+
+/**
+ * Exercices sur les nombres et les caluls mathématiques
+ */
+var calculPuissance = function (x, y) {
+
+}
+var valeurAbsolue = function (nombre) {
+
+}
+var valeurAbsolueArray = function (array) {
+
+}
+var sufaceCercle = function (rayon) {
+
+}
+var hypothenuse = function (ab, ac) {
+
+}
+var calculIMC = function (poids, taille) {
+
+}
