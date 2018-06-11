4cd9e5a54091effc00b51bb96740d2b75623e8e6
Antoni Dalle Nogare <antoni.dallenogare@gmail.com>
2018-06-11 16:32:15 +0200

    11/6 16h32

--- Série5/questions.js
+++ Série5/questions.js
@@ -28,37 +28,42 @@ var SupprEspaceString = function (texte) {
 return texte.trim();
 }
 var IsString = function (texte) {
+// if (texte = "") {
+// return true;
+// }
 
+typeof texte === "string";
+return true;
 }
 
 var AfficherExtensionString = function (texte) {
-
+return texte.split('.').pop();
 }
 var NombreEspaceString = function (texte) {
-
+ return texte.match(/[ ]/g).length;
 }
 var InverseString = function (texte) {
-
+return texte.split('').reverse('').join('');
 }
 
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
 var calculPuissance = function (x, y) {
-
+return Math.pow(x, y);
 }
 var valeurAbsolue = function (nombre) {
-
+return Math.abs(nombre);
 }
 var valeurAbsolueArray = function (array) {
-
+return array.map(Math.abs);
 }
 var sufaceCercle = function (rayon) {
-
+return Math.round(Math.PI * (rayon * rayon));
 }
 var hypothenuse = function (ab, ac) {
-
+return Math.sqrt(Math.pow(ab, 2)+Math.pow(ac, 2));
 }
 var calculIMC = function (poids, taille) {
-
+ return Number((poids / (Math.pow(taille, 2))).toFixed(2));
 }
